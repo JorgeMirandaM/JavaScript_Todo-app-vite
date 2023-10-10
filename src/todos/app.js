f815d80af31b76e2f37e03668f4b16bbed404786
JorgeMirandaM <jorge100099hotmail.com>
2023-10-10 17:46:39 -0600

    toggle todo with addEventListener implemented

--- src/todos/app.js
+++ src/todos/app.js
@@ -28,6 +28,7 @@ export const App = (elementId) => {
 
     //Refrencias HTML
     const newDescriptionInput= document.querySelector(ElementIDs.NewTodoInput);
+    const todoListUL = document.querySelector(ElementIDs.TodoList);
 
     //Listeners
     newDescriptionInput.addEventListener('keyup', (event)=>{
@@ -37,6 +38,12 @@ export const App = (elementId) => {
         todoStore.addTodo(event.target.value);
         displayTodos();
         event.target.value='';
+    });
+
+    todoListUL.addEventListener('click',(event)=>{
+        const element= event.target.closest('[data-id]')
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
     });
 
 }
